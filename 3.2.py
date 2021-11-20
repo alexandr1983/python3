8b877d602be0d388f173fcd0c38d260df5587b35
alexandr1983 <alexandr.mullin@gmail.com>
2021-11-20 16:43:29 +0300

    3.2.py

---
+++ 3.2.py
@@ -0,0 +1,6 @@
+def anketa(name, lastname, year, city, email, phone):
+    return print(f"Name: {name} Lastname: {lastname} Year of birthday: {year} City: {city} Email: {email} Phone: {phone}")
+
+
+anketa(name = input("Name: "), lastname = input("Lastname: "), year = input("Year of birthday: "),
+       city = input("City: "), email = input("email: "), phone = input("phone: "))
